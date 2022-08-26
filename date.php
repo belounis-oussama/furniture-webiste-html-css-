1dcb53328e6d0d9cfbf55c5d5473b6f2b0351e83
belounis-oussama <contact.elprofessordz@gmail.com>
2022-08-26 14:05:08 +0100

    final commit

---
+++ date.php
@@ -0,0 +1,5 @@
+<?php
+
+ //try to watch youtube video on how link php to data base 
+ //basically you need to take input and store them into your table
+?>
---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+const GC = Document.getElementById("GC");
+const WC = Document.getElementById("WC");
+const BC = Document.getElementById("BC");
+const GT = Document.getElementById("GT");
+const BT = Document.getElementById("BT");
+const WT = Document.getElementById("WT");
+const LD = Document.getElementById("LD");
+const SD = Document.getElementById("SD");
+GC.onclick = alert("vous avez ajouter gray chair au panier");
+WC.onclick = () => {alert("vous avez ajouter white chair au panier");};
+BC.onclick = () => {alert("vous avez ajouter black chair au panier");};
+GT.onclick = () => {alert("vous avez ajouter gray table au panier");};
+BT.onclick = () => {alert("vous avez ajouter black table au panier");};
+WT.onclick = () => {alert("vous avez ajouter white table au panier");};
+LD.onclick = () => {alert("vous avez ajouter long desk au panier");};
+SD.onclick = () => {alert("vous avez ajouter short desk au panier");};
+</script>
